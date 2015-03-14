0918a2b742d402d3deafc8c586ddbd84e770bc5c
Harsha <harsh.ssd@gmail.com>
2015-03-14 15:58:16 -0400

    modified model to save FB user

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,12 @@ var User = mongoose.Schema({
     local : {
         email : String,
         password : String    
+    },
+    facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
     }
 });
 
