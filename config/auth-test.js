2596135d2a7b4b224e39fa3cb6260950975cce35
Harsha <harsh.ssd@gmail.com>
2015-03-15 09:21:24 -0400

    config for heroku domain name

--- config/auth-test.js
+++ config/auth-test.js
@@ -4,7 +4,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '368696069984040', // your App ID
         'clientSecret'  : '08469bcae8e80a60eb519473929f0299', // your App Secret
-        'callbackURL'   : 'https://oauth-using-node.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
     }
 
 };
