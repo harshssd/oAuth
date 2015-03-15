8bfe57786a42f57d50c9398caab2b6d01fa20c18
Harsha <harsh.ssd@gmail.com>
2015-03-15 09:03:42 -0400

    mongo cloud

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 module.exports = {
 
-//    'url' : 'mongodb://root:root@ds047440.mongolab.com:47440/shared-expenses'
-    'url' : 'mongodb://localhost/test'
+    'url' : 'mongodb://root:root@ds047440.mongolab.com:47440/shared-expenses'
+//    'url' : 'mongodb://localhost/test'
 
 };
 
